13e740de6aae149b6d6dced78ea8c519f0066776
Giuliano Faustini <giuliperus@gmail.com>
2023-09-10 09:30:19 +0300

    Added update on backend server for API request /api/persons link to exercise 3.9

--- src/services/PersonService.js
+++ src/services/PersonService.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
   const getAll = () => {
     const request = axios.get(baseUrl)
@@ -11,7 +11,7 @@ const baseUrl = 'http://localhost:3001/persons'
 
 const nameTakeOut = (id) => {
 
-  const request = axios.delete(`http://localhost:3001/persons/${id}`)
+  const request = axios.delete(`http://localhost:3001/api/persons/${id}`)
   return request.then(response => response.data)
 }
 
@@ -21,7 +21,7 @@ const create = newPerson => {
 }
 
 const update = (id, newPerson) => {
-  const request = axios.put(`http://localhost:3001/persons/${id}`,  newPerson)
+  const request = axios.put(`http://localhost:3001/api/persons/${id}`,  newPerson)
   return request.then(response => response.data)
 
 }
