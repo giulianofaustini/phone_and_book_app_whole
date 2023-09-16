ad31e8be757a1077c6acca879d1640796811bd6c
Giuliano Faustini <giuliperus@gmail.com>
2023-09-16 09:42:48 +0300

    Added check if newName add is less than 3

--- src/components/AddName.jsx
+++ src/components/AddName.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
 
+import PersonService from "../services/PersonService";
 
-
-import PersonService from '../services/PersonService';
-
- const AddName = ({ newName, setNewName, persons, setPersons, handleNameChange, newNumber, setNewNumber, handleNumberChange, handleUpdate, handleMessageToGo, handleInfoMessageErrorUpdate }) => {
+const AddName = ({
+  newName,
+  setNewName,
+  persons,
+  setPersons,
+  handleNameChange,
+  newNumber,
+  setNewNumber,
+  handleNumberChange,
+  handleUpdate,
+  handleMessageToGo,
+  handleInfoMessageErrorUpdate,
+}) => {
   const addNameButton = (event) => {
     event.preventDefault();
 
@@ -18,49 +28,58 @@ import PersonService from '../services/PersonService';
       if (confirmUpdate) {
         const updatedPerson = {
           ...existingPerson,
-          number: newNumber
+          number: newNumber,
         };
 
         PersonService.update(existingPerson.id, updatedPerson)
           .then(() => {
-            const updatedPersons = persons.map(person =>
+            const updatedPersons = persons.map((person) =>
               person.id === existingPerson.id ? updatedPerson : person
             );
             setPersons(updatedPersons);
-            setNewName('');
-            setNewNumber('');
+            setNewName("");
+            setNewNumber("");
           })
-          .catch(error => {
-            handleInfoMessageErrorUpdate(`Info of ${updatedPerson.name} has been already deleted from the list.`, error);
-            setPersons(persons.filter((person) => person.id !== existingPerson.id))
-            setNewName('');
-            setNewNumber('');
+          .catch((error) => {
+            handleInfoMessageErrorUpdate(
+              `Info of ${updatedPerson.name} has been already deleted from the list.`,
+              error
+            );
+            setPersons(
+              persons.filter((person) => person.id !== existingPerson.id)
+            );
+            setNewName("");
+            setNewNumber("");
           });
       }
+    } else if (newName.length < 3) {
+      handleInfoMessageErrorUpdate(
+        `Person validation failed: Name "${newName}" is shorter than the minimum allowed length for the name (3).`
+      );
+      setNewName("");
+      setNewNumber("");
+   
     } else {
       const newPerson = {
         name: newName,
         number: newNumber,
-        id: persons.length + 1
+        id: persons.length + 1,
       };
 
-      PersonService
-        .create(newPerson, )
+      PersonService.create(newPerson)
         .then((createdPerson) => {
           setPersons(persons.concat(createdPerson));
-          handleMessageToGo(`Person ${newPerson.name} has been added to the list`);
-          setNewName('');
-          setNewNumber('');
-          
+          handleMessageToGo(
+            `Person ${newPerson.name} has been added to the list`
+          );
+          setNewName("");
+          setNewNumber("");
         })
         .catch((error) => {
-          console.error('Error adding person:', error);
-         
+          console.error("Error adding person:", error);
         });
     }
   };
-
-
 
   return (
     <div>
@@ -69,7 +88,7 @@ import PersonService from '../services/PersonService';
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-         number: <input value={newNumber} onChange={handleNumberChange}/>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit"> - click here to add a new name - </button>
